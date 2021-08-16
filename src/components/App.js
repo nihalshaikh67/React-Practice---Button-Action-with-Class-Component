338e2777c6417dd2c81eebe526acac2034b26a35
nihalshaikh67 <nihalshaikh834@gmail.com>
2021-08-16 12:45:56 +0530

    add

--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,19 @@ import '../styles/App.css';
 class App extends Component {
     constructor(props) {
 		super(props);
+		this.state={
+			flag : false,
+		}
 	};
 
     render() {
     	return(
     		<div id="main">
-				{ /* Do not remove this main div!! */ }
+				<button id="click" onClick={()=>{
+					this.setState({flag:true})
+				}}>Click me</button>
+
+				{this.state.flag && <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>}
     		</div>
     	);
     }
